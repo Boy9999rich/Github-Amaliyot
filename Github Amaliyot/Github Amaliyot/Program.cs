bc9eb052cd2b95f82fe346e246398004024a1ea4
Boy9999rich <boymurodmamarajapov1@gmail.com>
2024-11-30 12:57:20 +0500

    yuklash

---
+++ Github Amaliyot/Github Amaliyot/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Github_Amaliyot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
